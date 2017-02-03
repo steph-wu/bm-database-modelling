14dcf92b00af6e7a6799e7461733bd20634cdec6
STEPHANIE <WU.STEPHANIE@LIVE.COM>
2017-02-03 14:21:50 -0500

    Create and migrate articles and ingredients join table

--- recipe_app/db/schema.rb
+++ recipe_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170203191907) do
+ActiveRecord::Schema.define(version: 20170203192104) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string   "name"
@@ -24,6 +24,11 @@ ActiveRecord::Schema.define(version: 20170203191907) do
     t.decimal  "prep_time_hours"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+  end
+
+  create_table "recipes_ingredients", force: :cascade do |t|
+    t.integer "recipe_id"
+    t.integer "ingredient_id"
   end
 
 end
