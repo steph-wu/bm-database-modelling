d6814bdac34ab5bed323af6baf8105112ad39197
STEPHANIE <WU.STEPHANIE@LIVE.COM>
2017-02-03 14:59:48 -0500

    Create associations

--- appointment_app/app/models/appointment.rb
+++ appointment_app/app/models/appointment.rb
@@ -1,2 +1,6 @@
 class Appointment < ApplicationRecord
+
+  belongs_to :doctor
+  belongs_to :patient
+
 end
--- appointment_app/app/models/patient.rb
+++ appointment_app/app/models/patient.rb
@@ -1,2 +1,6 @@
 class Patient < ApplicationRecord
+
+  has_many :appointments
+  has_many :doctors, through: :appointments
+
 end
